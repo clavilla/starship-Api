5f755d414d97c41b85579b9ca314efd8f1648449
clavilla <cristianlavilla@hotmail.com>
2024-05-18 11:24:16 -0300

    Initial commit

---
+++ src/main/java/com/clavilla/w2wchallenge/StarshipApi/StarshipApiApplication.java
@@ -0,0 +1,13 @@
+package com.clavilla.w2wchallenge.StarshipApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarshipApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarshipApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clavilla/w2wchallenge/StarshipApi/StarshipApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clavilla.w2wchallenge.StarshipApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarshipApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
