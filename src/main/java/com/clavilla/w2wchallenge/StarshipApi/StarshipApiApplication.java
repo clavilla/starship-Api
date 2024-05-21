28cc721baa45e4771443ad907107a473052c94bf
clavilla <cristianlavilla@hotmail.com>
2024-05-21 11:28:20 -0300

    starship-Api Se agrego el cacheo de los servicios

--- src/main/java/com/clavilla/w2wchallenge/StarshipApi/StarshipApiApplication.java
+++ src/main/java/com/clavilla/w2wchallenge/StarshipApi/StarshipApiApplication.java
@@ -2,8 +2,10 @@ package com.clavilla.w2wchallenge.StarshipApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class StarshipApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/clavilla/w2wchallenge/StarshipApi/service/StarshipService.java
+++ src/main/java/com/clavilla/w2wchallenge/StarshipApi/service/StarshipService.java
@@ -6,6 +6,10 @@ import com.clavilla.w2wchallenge.StarshipApi.model.dto.StarshipRequestDto;
 import com.clavilla.w2wchallenge.StarshipApi.model.dto.StarshipResponseDto;
 import com.clavilla.w2wchallenge.StarshipApi.model.entity.Starship;
 import com.clavilla.w2wchallenge.StarshipApi.repository.StarshipRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +20,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Service
+@Slf4j
 public class StarshipService {
 
     private final StarshipRepository starshipRepository;
@@ -27,27 +32,34 @@ public class StarshipService {
     }
 
     @NonNull
+    @Cacheable(value = "starships", key = "#pageable.pageNumber + '_' + #pageable.pageSize + '_' + #pageable.sort.toString()")
     @Transactional(readOnly = true)
     public Page<StarshipResponseDto> findAll(Pageable pageable) {
         Page<Starship> starshipPage = starshipRepository.findAll(pageable);
+        log.info("Retrieved {} starships from data base", starshipPage.getTotalElements());
         return starshipMapper.convertToPagetDto(starshipPage);
     }
 
     @NonNull
+    @Cacheable(value = "starshipsById", key = "#id")
     @Transactional(readOnly = true)
     public StarshipResponseDto findById(@NonNull Long id) {
         Starship starship = starshipRepository.findById(id)
                 .orElseThrow(ResourceNotFoundException::new);
+        log.info("Retrieved starship with id: {} from data base", id);
         return starshipMapper.convertToDto(starship);
     }
 
-    @Transactional
+    @Cacheable(value = "starshipsByName", key = "#name")
+    @Transactional(readOnly = true)
     public List<StarshipResponseDto> findByNameContainingIgnoreCase(String name) {
         List<Starship> starshipList = starshipRepository.findByNameContainingIgnoreCase(name);
+        log.info("{} starships obtained from data base", starshipList.size());
         return starshipMapper.convertToListDto(starshipList);
     }
 
     @Transactional
+    @CacheEvict(value = {"starships", "starshipsByName", "starshipsById"}, allEntries = true)
     public StarshipResponseDto create(StarshipRequestDto starshipRequestDto) {
         Starship starship = starshipMapper.convertToEntity(starshipRequestDto);
         starship.setCreatedAt(LocalDate.now());
@@ -56,6 +68,8 @@ public class StarshipService {
     }
 
     @Transactional
+    @CacheEvict(value = {"starships", "starshipsByName", "starshipsById"}, allEntries = true)
+    @CachePut(value = "starshipsById", key = "#id")
     public StarshipResponseDto update(Long id, StarshipRequestDto starshipRequestDto) {
         Starship starship = starshipRepository.findById(id)
                 .orElseThrow(ResourceNotFoundException::new);
@@ -67,6 +81,7 @@ public class StarshipService {
     }
 
     @Transactional
+    @CacheEvict(value = {"starships", "starshipsByName", "starshipsById"}, allEntries = true)
     public void deleteById(@NonNull Long id) {
         starshipRepository.deleteById(id);
     }
